bccbe56ef0110b6929ca0595519a7a0a4228b9d7
Томас <39100726+ThomasMullagaliev@users.noreply.github.com>
2018-07-28 21:59:35 +0300

    Revert "Sass styles"

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
 var sass = require('gulp-sass');
 
-gulp.task('server', ['styles'], function() {
+gulp.task('server', ['sass'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./app/sass/**/*.scss', ['styles']);
+    // gulp.watch('./app/less/**/*.less', ['less']);
+    gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
 
+gulp.task('less', function() {
+    return gulp.src('./app/less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
+});
 
-gulp.task('styles', function() {
+gulp.task('sass', function() {
     return gulp.src('./app/sass/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./app/css'))
